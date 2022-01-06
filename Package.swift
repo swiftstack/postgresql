89b49bd4c4bcf2f0386122e8e939d3245fd01925
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:50:07 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "PostgreSQL",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "PostgreSQL",
